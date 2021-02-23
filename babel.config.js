7aecd13daa6ce1d2cdfae09b3dff0c1b90ff496f
Mohammed Essam <dev.mohammed.essam@gmial.com>
2021-02-23 23:33:45 +0200

    project initialization

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"]
+  presets: ["@vue/cli-plugin-babel/preset"],
+  plugins: [
+    [
+      "module-resolver",
+      {
+        root: ["."],
+        alias: {
+          "@Components": "./src/components",
+          "@Views": "./src/views",
+          "@Assets": "./src/assets"
+        }
+      }
+    ]
+  ]
 };
